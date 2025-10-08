bc2be06bdcd81940647e37775ff74778c5a3eb17
JakeS2028 <djspringer9@gmail.com>
2025-10-08 14:19:19 -0400

    Update hhi-exploration.R

--- code/hhi/hhi-exploration.R
+++ code/hhi/hhi-exploration.R
@@ -1,6 +1,13 @@
 # Load in setup
 source(here::here("code", "0-setup.R"))
 
-cars <- open_dataset(here('data_local', 'listings.parquet'))
+counts_30 <- open_dataset(here('data_local','counts', 'counts-2024','counts_60.parquet'))
 
-names(cars)
+counts_30 <- counts_30 %>% 
+    select(-price_bin)
+
+make_counts <- counts_30 %>% 
+    select(GEOID, n, make) %>% 
+    group_by(GEOID, make) %>% 
+    summarise(n = sum(n), .groups = "drop") %>% 
+    collect()
