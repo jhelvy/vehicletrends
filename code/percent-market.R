0d924aa099de761b07f310e04d189d75008fcde5
emse-p4a-gwu <john.helveston@gmail.com>
2025-10-31 11:11:41 -0400

    add p_market calculations

--- code/percent-market.R
+++ code/percent-market.R
@@ -1,255 +1,58 @@
-# Load functions, libraries, and other settings
-source(here::here("code", "0-setup.R"))
+source(here::here('code', '0-setup.R'))
 
-nrel_dt <- open_dataset(here::here("data", "nrel_data_budget.parquet")) %>%
-  filter(!is.na(budget), !is.na(pop_over_25), !is.na(income)) %>%
-  collect()
-tract_dt <- read_parquet(here('data', 'tract_dt.parquet'))
-pop <- nrel_dt %>%
-  distinct(GEOID, pop_over_25) %>%
-  group_by(GEOID) %>%
-  summarise(pop_over_25 = mean(pop_over_25))
+price_levels <- c(
+  "$0-$30k",
+  "$30k-$40k",
+  "$40k-$50k",
+  "$50k-$60k",
+  "$60k+"
+)
 
-ds_total <- open_dataset(here(
-  'data',
-  'counts-2024',
-  'counts_30.parquet'
-))
-
-# Powertrain analysis ----
-
-dt_summaries <- ds_total %>%
-  group_by(GEOID, powertrain) %>%
-  summarise(n = sum(n)) %>%
-  collect() %>%
-  group_by(GEOID) %>%
-  mutate(p = n / sum(n)) %>%
-  clean_factors_powertrain()
-
-dt_summaries %>%
-  ggplot() +
-  geom_boxplot(
-    aes(
-      x = p,
-      y = powertrain
-    ),
-    width = 0.5,
-    outlier.shape = NA
-  ) +
-  scale_x_continuous(
-    breaks = seq(0, 1, 0.2),
-    limits = c(0, 1),
-    labels = scales::percent
-  ) +
-  theme_minimal_vgrid(font_family = font, font_size = 16) +
-  theme(
-    strip.background = element_rect("grey80"),
-    plot.title.position = "plot",
-    panel.background = element_rect(fill = 'white', color = NA),
-    plot.background = element_rect(fill = 'white', color = NA),
-    legend.position = 'none'
-  ) +
-  panel_border() +
-  labs(
-    x = 'Share of all local listings',
-    y = 'Powertrain',
-    title = 'Share of local vehicle listings by powertrain'
+# Open
+ds <- load_ds() %>%
+  filter(inventory_type == 'new') %>%
+  filter(listing_year %in% c(2018, 2024)) %>%
+  mutate(
+    price_bin = case_when(
+      price < 30000 ~ "$0-$30k",
+      price >= 30000 & price < 40000 ~ "$30k-$40k",
+      price >= 40000 & price < 50000 ~ "$40k-$50k",
+      price >= 50000 & price < 60000 ~ "$50k-$60k",
+      price >= 60000 ~ "$60k+",
+      TRUE ~ "Other"
+    )
   )
 
-# ggsave(
-#   here("figs", "percent-market-powertrain.png"),
-#   height = 5,
-#   width = 8
-# )
+# Price bin by powertrain - compute counts by powertrain
+get_dumbbell_data <- function(ds, var1, var2) {
+  dumbbell_data <- ds %>%
+    count(listing_year, {{ var1 }}, {{ var2 }}) %>%
+    collect() %>%
+    group_by(listing_year, {{ var1 }}) %>%
+    mutate(p = n / sum(n)) %>%
+    ungroup() %>%
+    select(-n) %>%
+    pivot_wider(
+      names_from = listing_year,
+      values_from = p,
+      names_prefix = "year_"
+    ) %>%
+    mutate(
+      year_2018 = ifelse(is.na(year_2018), 0, year_2018),
+      year_2024 = ifelse(is.na(year_2024), 0, year_2024)
+    )
+  return(dumbbell_data)
+}
 
-# Powertrain by price bin ----
-
-dt_summaries <- ds_total %>%
-  group_by(GEOID, powertrain, price_bin) %>%
-  summarise(n = sum(n)) %>%
-  collect() %>%
-  group_by(GEOID, price_bin) %>%
-  mutate(p = n / sum(n)) %>%
-  clean_factors_powertrain()
-
-dt_summaries %>%
-  ggplot() +
-  geom_boxplot(
-    aes(
-      x = p,
-      y = powertrain,
-      fill = price_bin
-    ),
-    width = 0.5,
-    outlier.shape = NA
-  ) +
-  scale_x_continuous(
-    breaks = seq(0, 1, 0.2),
-    limits = c(0, 1),
-    labels = scales::percent
-  ) +
-  guides(color = guide_legend(reverse = TRUE)) +
-  theme_minimal_vgrid(font_family = font, font_size = 16) +
-  theme(
-    strip.background = element_rect("grey80"),
-    plot.title.position = "plot",
-    panel.background = element_rect(fill = 'white', color = NA),
-    plot.background = element_rect(fill = 'white', color = NA)
-  ) +
-  panel_border() +
-  labs(
-    fill = 'Price bin',
-    x = 'Share of all local listings',
-    y = 'Powertrain',
-    title = 'Share of local vehicle listings by powertrain'
-  )
-
-# Vehicle type analysis ----
-
-dt_summaries <- ds_total %>%
-  group_by(GEOID, vehicle_type) %>%
-  summarise(n = sum(n)) %>%
-  collect() %>%
-  group_by(GEOID) %>%
-  mutate(p = n / sum(n)) %>%
-  clean_factors_vehicle_type()
-
-dt_summaries %>%
-  ggplot() +
-  geom_boxplot(
-    aes(
-      x = p,
-      y = vehicle_type
-    ),
-    width = 0.5,
-    outlier.shape = NA
-  ) +
-  scale_x_continuous(
-    breaks = seq(0, 1, 0.2),
-    limits = c(0, 1),
-    labels = scales::percent
-  ) +
-  theme_minimal_vgrid(font_family = font, font_size = 16) +
-  theme(
-    strip.background = element_rect("grey80"),
-    plot.title.position = "plot",
-    panel.background = element_rect(fill = 'white', color = NA),
-    plot.background = element_rect(fill = 'white', color = NA),
-    legend.position = 'none'
-  ) +
-  panel_border() +
-  labs(
-    x = 'Share of all local listings',
-    y = 'Vehicle Type',
-    title = 'Share of local vehicle listings by vehicle type'
-  )
-
-# ggsave(
-#   here("figs", "percent-market-vehicle-type.png"),
-#   height = 5,
-#   width = 8
-# )
-
-# Powertrain - urbanicity ----
-
-dt_summaries <- ds_total %>%
-  left_join(
-    tract_dt %>%
-      select(GEOID, class),
-    by = 'GEOID'
-  ) %>%
-  group_by(GEOID, class, powertrain) %>%
-  summarise(n = sum(n)) %>%
-  collect() %>%
-  group_by(GEOID, class) %>%
-  mutate(p = n / sum(n)) %>%
-  clean_factors_powertrain()
-
-dt_summaries %>%
-  ggplot() +
-  geom_boxplot(
-    aes(
-      x = p,
-      y = powertrain,
-      fill = class
-    ),
-    width = 0.5,
-    outlier.shape = NA
-  ) +
-  scale_x_continuous(
-    breaks = seq(0, 1, 0.2),
-    limits = c(0, 1),
-    labels = scales::percent
-  ) +
-  theme_minimal_grid(font_family = font, font_size = 16) +
-  theme(
-    strip.background = element_rect("grey80"),
-    plot.title.position = "plot",
-    panel.background = element_rect(fill = 'white', color = NA),
-    plot.background = element_rect(fill = 'white', color = NA)
-  ) +
-  panel_border() +
-  labs(
-    fill = 'Urbanicity',
-    x = 'Share of all local listings',
-    y = 'Powertrain',
-    title = 'Share of local vehicle listings by powertrain'
-  )
-
-# ggsave(
-#   here("figs", "percent-market-powertrain-urbanicity.png"),
-#   height = 5,
-#   width = 8
-# )
-
-# Vehicle type - urbanicity ----
-
-dt_summaries <- ds_total %>%
-  left_join(
-    tract_dt %>%
-      select(GEOID, class),
-    by = 'GEOID'
-  ) %>%
-  group_by(GEOID, class, vehicle_type) %>%
-  summarise(n = sum(n)) %>%
-  collect() %>%
-  group_by(GEOID, class) %>%
-  mutate(p = n / sum(n)) %>%
-  clean_factors_vehicle_type()
-
-dt_summaries %>%
-  ggplot() +
-  geom_boxplot(
-    aes(
-      x = p,
-      y = vehicle_type,
-      fill = class
-    ),
-    width = 0.5,
-    outlier.shape = NA
-  ) +
-  scale_x_continuous(
-    breaks = seq(0, 1, 0.2),
-    limits = c(0, 1),
-    labels = scales::percent
-  ) +
-  theme_minimal_vgrid(font_family = font, font_size = 16) +
-  theme(
-    strip.background = element_rect("grey80"),
-    plot.title.position = "plot",
-    panel.background = element_rect(fill = 'white', color = NA),
-    plot.background = element_rect(fill = 'white', color = NA)
-  ) +
-  panel_border() +
-  labs(
-    fill = 'Urbanicity',
-    x = 'Share of all local listings',
-    y = 'Vehicle type',
-    title = 'Share of local vehicle listings by vehicle type'
-  )
-
-# ggsave(
-#   here("figs", "percent-market-vehicle-type-urbanicity.png"),
-#   height = 5,
-#   width = 8
-# )
+get_dumbbell_data(ds, powertrain, vehicle_type) %>%
+  write_csv(here('data', 'p_market_powertrain_vehicle_type.csv'))
+get_dumbbell_data(ds, powertrain, price_bin) %>%
+  write_csv(here('data', 'p_market_powertrain_price_bin.csv'))
+get_dumbbell_data(ds, vehicle_type, powertrain) %>%
+  write_csv(here('data', 'p_market_vehicle_type_powertrain.csv'))
+get_dumbbell_data(ds, vehicle_type, price_bin) %>%
+  write_csv(here('data', 'p_market_vehicle_type_price_bin.csv'))
+get_dumbbell_data(ds, price_bin, powertrain) %>%
+  write_csv(here('data', 'p_market_price_bin_powertrain.csv'))
+get_dumbbell_data(ds, price_bin, vehicle_type) %>%
+  write_csv(here('data', 'p_market_price_bin_vehicle_type.csv'))
